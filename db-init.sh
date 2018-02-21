de98354d2ff49b98984b45f1c3fc05ab33626209
ragyun <faithfulrh@gmail.com>
2018-02-21 23:15:41 +0900

    Update db-init.sh

--- db-init.sh
+++ db-init.sh
@@ -2,10 +2,10 @@
 set -e
 
 mkdir -pv "/var/lib/postgresql/db-data/adcsmart_generals"
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE TABLESPACE dbspace LOCATION '/var/lib/postgresql/db-data/adcsmart_generals'"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE TABLESPACE adcsmart_generals OWNER adcsmartdba  LOCATION '/var/lib/postgresql/db-data/adcsmart_generals'"
 
 mkdir -pv "/var/lib/postgresql/db-data/adcsmart_indexes"
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE TABLESPACE dbspace LOCATION '/var/lib/postgresql/db-data/adcsmart_indexes'"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE TABLESPACE adcsmart_indexes OWNER adcsmartdba LOCATION '/var/lib/postgresql/db-data/adcsmart_indexes'"
 
 mkdir -pv "/var/lib/postgresql/db-data/adcsmart_logs"
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE TABLESPACE dbspace LOCATION '/var/lib/postgresql/db-data/adcsmart_logs'"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE TABLESPACE adcsmart_logs OWNER adcsmartdba LOCATION '/var/lib/postgresql/db-data/adcsmart_logs'"
