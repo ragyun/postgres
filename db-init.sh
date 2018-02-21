fe888a12f087a2a40989b9284f2303106659e9f3
ragyun <faithfulrh@gmail.com>
2018-02-21 22:58:09 +0900

    Update db-init.sh

--- db-init.sh
+++ db-init.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-for tablespace in adcsmart_generals adcsmart_indexes adcsmart_logs; do
-	mkdir -pv "/var/lib/postgresql/db-data/$tablespace"
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE TABLESPACE dbspace LOCATION '/var/lib/postgresql/db-data/$tablespace'"
-done
+mkdir -pv "/var/lib/postgresql/db-data/adcsmart_generals"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE TABLESPACE dbspace LOCATION '/var/lib/postgresql/db-data/adcsmart_generals'"
+
+mkdir -pv "/var/lib/postgresql/db-data/adcsmart_indexes"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE TABLESPACE dbspace LOCATION '/var/lib/postgresql/db-data/adcsmart_indexes'"
+
+mkdir -pv "/var/lib/postgresql/db-data/adcsmart_logs"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE TABLESPACE dbspace LOCATION '/var/lib/postgresql/db-data/adcsmart_logs'"
