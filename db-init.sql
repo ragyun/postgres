fb381432847d5e785dfea2f1d3ee41388f49316b
ragyun <faithfulrh@gmail.com>
2018-02-21 17:44:56 +0900

    Create db-init.sql

---
+++ db-init.sql
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+for tablespace in adcsmart_generals adcsmart_indexes adcsmart_logs; do
+	mkdir -pv "/var/lib/postgresql/db-data/$tablespace"
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE TABLESPACE dbspace LOCATION '/db-data/$tablespace'"
+done
